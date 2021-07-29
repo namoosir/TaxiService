2c0d792032850589caa017285024dbf61020101d
Mutasem Kharsa <mutasem.kharsa@mail.utoronto.ca>
2021-07-28 22:09:39 -0400

    starter files

---
+++ ApiGateway/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -0,0 +1,9 @@
+package ca.utoronto.utm.mcs;
+
+public class App {
+   static int PORT = 8000;
+
+   public static void main(String[] args) {
+      System.out.printf("Server started on port %d...\n", PORT);
+   }
+}
