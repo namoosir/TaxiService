71948f51d767c3c1a77026780a546ba8e5ab3c94
namoosir <saqeebnazmus@gmail.com>
2021-08-01 22:18:22 -0400

    GatewayFeature add feature

--- ApiGateway/src/main/java/ca/utoronto/utm/mcs/App.java
+++ ApiGateway/src/main/java/ca/utoronto/utm/mcs/App.java
@@ -1,9 +1,20 @@
 package ca.utoronto.utm.mcs;
 
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class App {
    static int PORT = 8000;
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws IOException, ClassNotFoundException{
+      HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
+      server.createContext("/location", new Location());
+      server.createContext("/trip", new Trip());
+      server.createContext("/user", new User());
+      server.start();
       System.out.printf("Server started on port %d...\n", PORT);
    }
 }
